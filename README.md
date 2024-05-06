8b54602ecd590e9905cac71bca307d332e2fe867
RychardProficional <rychardproficional@gmail.com>
2024-05-06 06:21:07 -0400

    estilizando homepage

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Login
 
 Temos aqui um login feito com react e express
+## Dependências
+
+#### Aqui estão as dependencias do frontend do projeto:
+
+![Jest-Dom](https://img.shields.io/badge/Jest--Dom-%5E5.17.0-orange?style=flat-square&logo=jest)
+![Testing-Library](https://img.shields.io/badge/Testing--Library-%5E13.4.0-blue?style=flat-square&logo=testing-library)
+![User-Event](https://img.shields.io/badge/User--Event-%5E13.5.0-green?style=flat-square&logo=testing-library)
+![React](https://img.shields.io/badge/React-%5E18.2.0-blue?style=flat-square&logo=react)
+![React-Dom](https://img.shields.io/badge/React--Dom-%5E18.2.0-yellow?style=flat-square&logo=react)
+![React-Router-Dom](https://img.shields.io/badge/React--Router--Dom-%5E6.23.0-purple?style=flat-square&logo=react-router)
+![React-Scripts](https://img.shields.io/badge/React--Scripts-5.0.1-blue?style=flat-square&logo=react)
+![Web-Vitals](https://img.shields.io/badge/Web--Vitals-%5E2.1.4-red?style=flat-square&logo=web)
--- frontend/src/pages/homepage.js
+++ frontend/src/pages/homepage.js
@@ -1,16 +1,133 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #f8f9fa; /* Cinza claro */
+  height: 100vh;
+`;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  background-color: #0d6efd; /* Azul */
+  width: 100%;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1); /* Sombreamento */
+`;
+
+const HeaderTitle = styled.h1`
+  font-size: 2em;
+  color: #ffffff; /* Branco */
+`;
+
+const HeaderLinks = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const HeaderLink = styled(Link)`
+  text-decoration: none;
+  margin-left: 20px;
+  padding: 10px 20px;
+  font-size: 1.2em;
+  font-family: 'Arial', sans-serif;
+  font-weight: bold;
+  color: #ffffff; /* Branco */
+  background-color: #0d6efd; /* Azul */
+  border-radius: 8px;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #0a58ca; /* Azul mais escuro */
+  }
+`;
+
+const Section = styled.section`
+  margin-top: 30px;
+  background-color: #ffffff; /* Branco */
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.1); /* Sombreamento */
+`;
+
+const SectionTitle = styled.h3`
+  color: #0d6efd;
+  font-size: 1.5em;
+  margin-bottom: 10px;
+`;
+
+const Dependencies = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  max-width: 620px;
+`;
+
+const DependencyBadge = styled.img`
+  margin: 10px;
+`;
+
+const Text = styled.div`
+  color: #0d6efd;
+  font-size: 1.5em;
+`
+
+function FrontendDependencies() {
+  return (
+    <Section>
+      <SectionTitle>Dependências do Frontend</SectionTitle>
+      <Dependencies>
+        <DependencyBadge src="https://img.shields.io/badge/-Jest-orange?logo=jest" alt="Jest"/>
+        <DependencyBadge src="https://img.shields.io/badge/-Testing%20Library-blue?logo=testing-library" alt="Testing Library"/>
+        <DependencyBadge src="https://img.shields.io/badge/-User%20Event-green?logo=testing-library" alt="User Event"/>
+        <DependencyBadge src="https://img.shields.io/badge/-React-blue?logo=react" alt="React"/>
+        <DependencyBadge src="https://img.shields.io/badge/-React%20Dom-yellow?logo=react" alt="React Dom"/>
+        <DependencyBadge src="https://img.shields.io/badge/-React%20Router%20Dom-purple?logo=react-router" alt="React Router Dom"/>
+        <DependencyBadge src="https://img.shields.io/badge/-React%20Scripts-blue?logo=react" alt="React Scripts"/>
+        <DependencyBadge src="https://img.shields.io/badge/-Web%20Vitals-red?logo=web" alt="Web Vitals"/>
+      </Dependencies>
+    </Section>
+  );
+}
+
+function BackendDependencies() {
+  return (
+    <Section>
+      <SectionTitle>Dependências do Backend</SectionTitle>
+      <Dependencies>
+      <DependencyBadge src="https://img.shields.io/badge/-Cors-lightgrey?logo=cors" alt="Cors"/>
+        <DependencyBadge src="https://img.shields.io/badge/-Dotenv-green?logo=dotenv" alt="Dotenv"/>
+        <DependencyBadge src="https://img.shields.io/badge/-Express-blue?logo=express" alt="Express"/>
+        <DependencyBadge src="https://img.shields.io/badge/-Express%20MySQL%20Session-lightblue?logo=mysql" alt="Express MySQL Session"/>
+        <DependencyBadge src="https://img.shields.io/badge/-Express%20Session-lightblue?logo=express-session" alt="Express Session"/>
+        <DependencyBadge src="https://img.shields.io/badge/-MySQL-blue?logo=mysql" alt="MySQL"/>
+      </Dependencies>
+    </Section>
+  );
+}
 
 function Homepage() {
   return (
-    <div>
-      <Link to={"/login"}>login</Link>
-      <Link to={"/register"}>Cadastra-se</Link>
-      <div>
-        bem vindo a pagina de login feito em react
-      </div>
-    </div>
+    <>
+      <Header>
+        <HeaderTitle>Login</HeaderTitle>
+        <HeaderLinks>
+          <HeaderLink to={"/login"}>Entrar</HeaderLink>
+          <HeaderLink to={"/register"}>Cadastrar</HeaderLink>
+        </HeaderLinks>
+      </Header>
+      <Main>
+        <Text>Este site é feito com <strong>React</strong> no Frontend e <strong>Nodejs</strong> no Backend, utilizando o conceito de microserviços</Text>
+        <BackendDependencies />
+        <FrontendDependencies />
+      </Main>
+    </>
   );
 }
 
