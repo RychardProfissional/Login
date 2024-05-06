42d41d672f6ba0f0702f18be801622f341792b12
RychardProficional <rychardproficional@gmail.com>
2024-05-06 07:07:25 -0400

    começando estillização do login

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,5 +1,57 @@
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
+import styled from "styled-components"
+
+const Main = styled.main`
+    background-color: #0d6efd;
+    border-radius: 10px;
+    padding: 10px 20px;
+`
+    
+const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+`
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    height: 200px;
+    justify-content: space-evenly;
+`
+
+const LabelInput = styled.span`
+    position: absolute;
+    top: 50%;
+    left: 10px;
+    transform: translateY(-50%);
+    
+    pointer-events: none;
+    transition: .5s;
+`
+
+const Input = styled.input`
+    background-color: transparent;
+    border: none;
+    outline: none;
+`
+    
+const ContentInput = styled.label`
+    position: relative;
+    padding: 15px 20px 10px 20px;
+    cursor: text;
+    border-radius: 10px;
+    
+    &:hover ${LabelInput}, &:focus-within ${LabelInput}, & ${Input}:valid + ${LabelInput}{
+        top: 5px;
+        left: 7px;
+        transform: none;
+        font-size: 0.8em;
+    }
+`
 
 export default function Login() {
     const [email, setEmail] = useState('')
@@ -37,37 +89,37 @@ export default function Login() {
     }
 
     return (
-        <main>
-            <div>
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor='email'>
-                        <span>Email: </span>
-                        <input 
+        <Content>
+            <Main>
+                <Form onSubmit={handleSubmit}>
+                    <ContentInput htmlFor='email'>
+                        <Input 
                             id="email" 
                             name="email" 
                             type="text" 
                             value={email}
                             onChange={e => setEmail(e.target.value)}
-                            
+                            required  
                         />
-                    </label>
-                    <label htmlFor='password'>
-                        <span>Senha: </span>
-                        <input 
+                        <LabelInput>Email: </LabelInput>
+                    </ContentInput>
+                    <ContentInput htmlFor='password'>
+                        <Input 
                             id="password" 
                             name="password" 
                             type="password" 
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
-                            
+                            required
                         />
-                    </label>
+                        <LabelInput>Senha: </LabelInput>
+                    </ContentInput>
 
                     <button type="submit">Entrar</button>
-                </form>
+                </Form>
+            </Main>
 
-                <Link to={"/register"}>Cadastrar-se</Link>
-            </div>
-        </main>
+            <Link to={"/register"}>Cadastrar-se</Link>
+        </Content>
     )
 }
